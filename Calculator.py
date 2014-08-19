fbc6c1263acaf7067ae7a268e5b0382a5d7685bf
wimuhlbach <applesarefum@gmail.com>
2014-08-19 07:20:39 -0400

    calculator???
    random, boredom, other descriptive words

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+print ('Hello!')
+operation = input ('What operation would you like completed? + - * /')
+if operation == +
+
+else
+    if operation == -
+
+    else
+        if operation == *
+
+        else
+            if operation == /
+            
+              else print (''sorry, I can't
