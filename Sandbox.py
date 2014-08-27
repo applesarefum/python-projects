75da39bc5edf2bdb73573cb5b7b21eb24800c178
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-08-27 16:40:06 -0400

    def attempt
    ROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOMS

--- Sandbox.py
+++ Sandbox.py
@@ -1,4 +1,8 @@
 name = input ("Who are you?").title ().strip()
 password = input ("What's your Password?")
 for count in range(3):
-if name == ("Will") or ("William") and password == ("
+if name == ("Will") or ("William") and password == ()
+                                                    
+def login():
+    print('Welcome, William')
+                                                    
