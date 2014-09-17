e1b495511c007a8e832575e82986b88d5185319e
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-09-17 17:15:38 -0400

    libs and folders and names, OH MY!!
    lib test W00T
    Folders in GitHub... ALSO W00T
    other stuffs....... W00T

---
+++ lib-test/name.py
@@ -0,0 +1,9 @@
+def namelel():
+    name = input('lel. wetz ure naem!')
+    print("ure naem iz {} lel".format(name))
+
+def name():
+    if name == None:
+        print("WRONG")
+    else:
+        print('lel, nao u shud tiyp namelel and pres entur')
---
+++ lib-test/sweg_lel.py
@@ -0,0 +1,19 @@
+def sweg():
+    if swog == 0:
+        input("you don't understand what you have done, mortal")
+        input("you have unleashed power not meant for this world")
+        input("good luck...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        input("...")
+        swog = 1
+    else:
+        input("sweg")
+        sweg()
