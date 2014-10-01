cbc87c7e8d6046354162c38415fc09daa9d5450e
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-10-01 17:16:44 -0400

    more turtles
    umm is too jew
    ANIME TURTLES
    KLEIN, ASUNA, KIRITOOOOOOOOOOOOOOO

--- anything but turtle.py
+++ anything but turtle.py
@@ -1,19 +1,39 @@
 from turtle import Turtle
-jason = Turtle()
-senora_sad_burritto = Turtle()
-#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
-jason.pensize(5)
-jason.pencolor('cyan')
+
 def random_color():
     return (random(),random(),random())
 
 #lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+senora_sad_burritto = Turtle()
+senora_sad_burritto.pensize(5)
+senora_sad_burritto.pencolor(random_color())
+
+jason = Turtle()
+jason.pensize(5)
+jason.pencolor('cyan')
+
+asuna = Turtle()
+asuna.pensize(asize)
+asuna.pencolor(acolor)
+
+kirito = Turtle()
+asuna.pensize(asize)
+asuna.pencolor(acolor)
+
+klein = Turtle()
+asuna.pensize(asize)
+asuna.pencolor(acolor)
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
 for count in range(3):
     senora_sad_burritto.forward(250)
     senora_sad_burritto.right(120)
-for count in range (
+for count in range(6):
+
+for count in range():
+    klein.
 while True:
-    random_color
-    jason.forward(100)
-    jason.right(90)
-    
+    for count in range(4):
+        random_color
+        jason.forward(100)
+        jason.right(90)
+    jason.left(5)
