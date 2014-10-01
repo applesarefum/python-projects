67c317754ee87953455b00d997f0d8a6f376dc42
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-10-01 16:50:23 -0400

    turtles
    FROM FANDOM IMPORT RANDOM

---
+++ anything but turtle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+jason = Turtle()
+senora_sad_burritto = Turtle()
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+jason.pensize(5)
+jason.pencolor('cyan')
+def random_color():
+    return (random(),random(),random())
+
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+for count in range(3):
+    senora_sad_burritto.forward(250)
+    senora_sad_burritto.right(120)
+for count in range (
+while True:
+    random_color
+    jason.forward(100)
+    jason.right(90)
+    
