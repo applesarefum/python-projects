f6cbec2ef2d302bd21b99435c502d09420cb9ea8
wimuhlbach <applesarefum@gmail.com>
2014-10-01 22:43:15 -0400

    TETRA
    ...yeah, I have no idea what tetra means...
    i have invalid syntax somewhere, so if you see anything leave me a
    comment

--- anything but turtle.py
+++ anything but turtle.py
@@ -1,39 +1,56 @@
 from turtle import Turtle
-
+from random import random
 def random_color():
     return (random(),random(),random())
-
 #lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
 senora_sad_burritto = Turtle()
-senora_sad_burritto.pensize(5)
+senora_sad_burritto.pensize(random())
 senora_sad_burritto.pencolor(random_color())
 
 jason = Turtle()
 jason.pensize(5)
 jason.pencolor('cyan')
-
-asuna = Turtle()
-asuna.pensize(asize)
-asuna.pencolor(acolor)
-
-kirito = Turtle()
-asuna.pensize(asize)
-asuna.pencolor(acolor)
-
-klein = Turtle()
-asuna.pensize(asize)
-asuna.pencolor(acolor)
 #lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
-for count in range(3):
-    senora_sad_burritto.forward(250)
-    senora_sad_burritto.right(120)
-for count in range(6):
+def saovar():
+    acolor=input('Asuna pen color?')
+    asize=input('Asuna pensize?')
+    asuna = Turtle()
+    asuna.pensize(asize)
+    asuna.pencolor(acolor)
 
-for count in range():
-    klein.
-while True:
-    for count in range(4):
-        random_color
-        jason.forward(100)
-        jason.right(90)
-    jason.left(5)
+    kicolor=input('Kirito pen color?')
+    kisize=input('Kirito pensize?')
+    kirito = Turtle()
+    kirito.pensize(kisize)
+    kirito.pencolor(kicolor)
+
+    klcolor=input('Klein pen color?')
+    klsize=input('Klein pensize?')
+    klein = Turtle()
+    klein.pensize(klsize)
+    klein.pencolor(klcolor)
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+def saoturtle():
+    for count in range(6):
+        asuna.forward(107)
+        asuna.right(60)
+    for count in range(9):
+        klein.forward(99)
+        klein.left(40)
+    kirito.forward(72)
+    while True:
+        kirito.forward(20)
+        kirito.right(5)
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+def spirograph():
+    while True:
+        for count in range(4):
+            jason.color(random_color)
+            jason.forward(100)
+            jason.right(90)
+        jason.left(5)
+#lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel lel
+def sadburritto():
+    for count in range(3):
+        senora_sad_burritto.forward(250)
+        senora_sad_burritto.right(120)
