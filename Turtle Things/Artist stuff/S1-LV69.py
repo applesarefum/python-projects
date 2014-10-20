6f33774473616c4beb7651134a544291f1a9f3a1
wimuhlbach <applesarefum@gmail.com>
2014-10-20 17:51:37 -0400

    Artist things
    FIRE FIRE FIRE
    WHERE IS SHANE?
    FIRE FIRE FIRE
    WE FOUND SHANE

---
+++ Turtle Things/Artist stuff/S1-LV69.py
@@ -0,0 +1,34 @@
+from turtle import Turtle
+jerome = Turtle()
+jerome.pensize(1)
+#V1
+def potato():
+    for count in range(100):
+        jerome.forward(count)
+        jerome.right(100)
+#V2
+def banana():
+    for count in range(100):
+        jerome.forward(count)
+        jerome.right(90)
+#V3
+def ponana():
+    for count in range(50):
+        jerome.forward(count)
+        jerome.right(91)
+#V4
+def unicorntears():
+    for count in range(50):
+        jerome.forward(count)
+        jerome.right(count)
+#V5
+def blazingpineapples():
+    for count in range(50):
+        jerome.forward(count)
+        jerome.right(666)
+#V6
+def seahorse():
+    pass
+#V7
+def insertrandomwordhere():
+    pass
